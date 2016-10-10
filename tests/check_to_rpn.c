e11971d896f9d573758c9598e2de26eb14042f3a
Zack Brown <zbrown@pillartechnology.com>
2016-10-10 12:24:41 -0400

    Failing test for b+c = bc+

--- tests/check_to_rpn.c
+++ tests/check_to_rpn.c
@@ -3,11 +3,19 @@
 #include "../src/converter.h"
 #include "check_to_rpn.h"
 
-START_TEST(to_rpn_takes_turns_a_plus_b_into_a_b_plus)
+START_TEST(to_rpn_turns_a_plus_b_into_a_b_plus)
 {
     char buf[256] = { };
     to_rpn("a+b", buf);
     ck_assert_str_eq(buf, "ab+");
+}
+END_TEST
+
+START_TEST(to_rpn_turns_b_plus_c_into_b_c_plus)
+{
+    char buf[256] = { };
+    to_rpn("b+c", buf);
+    ck_assert_str_eq(buf, "bc+");
 }
 END_TEST
 
@@ -18,7 +26,8 @@ Suite *to_rpn_suite(void){
     tc = tcase_create("Core");
     s = suite_create("TO_RPN"); 
 
-    tcase_add_test(tc, to_rpn_takes_turns_a_plus_b_into_a_b_plus);
+    tcase_add_test(tc, to_rpn_turns_a_plus_b_into_a_b_plus);
+    tcase_add_test(tc, to_rpn_turns_b_plus_c_into_b_c_plus);
 
     suite_add_tcase(s, tc);
 
