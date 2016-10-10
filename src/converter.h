ec2ee8530cf86027ac9e04a17c9dc732230b1c86
Zack Brown <zbrown@pillartechnology.com>
2016-10-10 08:53:01 -0400

    pass a + b equals ab+

---
+++ src/converter.h
@@ -0,0 +1,6 @@
+#ifndef CONVERTER_H
+#define CONVERTER_H
+
+void to_rpn(const char *infix, char *buf); 
+
+#endif // CONVERTER_H
--- tests/check_to_rpn.c
+++ tests/check_to_rpn.c
@@ -1,11 +1,26 @@
 #include <stdlib.h>
 #include <check.h>
+#include "../src/converter.h"
 #include "check_to_rpn.h"
+
+START_TEST(to_rpn_takes_turns_a_plus_b_into_a_b_plus)
+{
+    char buf[256] = { };
+    to_rpn("a+b", buf);
+    ck_assert_str_eq(buf, "ab+");
+}
+END_TEST
 
 Suite *to_rpn_suite(void){
     Suite *s; 
+    TCase *tc;
 
+    tc = tcase_create("Core");
     s = suite_create("TO_RPN"); 
+
+    tcase_add_test(tc, to_rpn_takes_turns_a_plus_b_into_a_b_plus);
+
+    suite_add_tcase(s, tc);
 
     return s;
 }	
