1fcff945c3be0dadd681c29203fa6a72a8087c54
Zack Brown <zackdangerbrown@gmail.com>
2016-10-21 01:09:43 +0000

    failing test for a + b - c -> abc-+

--- tests/check_to_rpn.c
+++ tests/check_to_rpn.c
@@ -27,6 +27,14 @@ START_TEST(to_rpn_turns_a_plus_b_plus_c_into_a_b_plus_c_plus)
 }
 END_TEST
 
+START_TEST(to_rpn_turns_a_plus_b_minus_c_into_a_b_c_minus_plus)
+{
+    char buf[256] = { };
+    to_rpn("a+b-c", buf);
+    ck_assert_str_eq(buf, "abc-+");
+}
+END_TEST
+
 Suite *to_rpn_suite(void){
     Suite *s; 
     TCase *tc;
@@ -37,6 +45,7 @@ Suite *to_rpn_suite(void){
     tcase_add_test(tc, to_rpn_turns_a_plus_b_into_a_b_plus);
     tcase_add_test(tc, to_rpn_turns_b_plus_c_into_b_c_plus);
     tcase_add_test(tc, to_rpn_turns_a_plus_b_plus_c_into_a_b_plus_c_plus);
+    tcase_add_test(tc, to_rpn_turns_a_plus_b_minus_c_into_a_b_c_minus_plus);
 
     suite_add_tcase(s, tc);
 
